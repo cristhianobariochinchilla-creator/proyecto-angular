07e74cd72db46c4e51da18471bf21a3f9645be95
cristhian <cristhianobariochinchilla@gmail.com>
2025-09-21 13:38:20 -0400

    feat(iu): showcase template components on /iu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,16 +2,4 @@ import { Routes } from '@angular/router';
 import { Layout } from './layout/layout';
 import { Home } from './home/home';
 
-export const routes: Routes = [
-{
-    path:'',
-    component:Layout,
-    children:[
-        {
-            path:'',
-            component:Home,
-            title:'INICIO'
-        }
-    ]
-}
-];
+export const routes: Routes = [];
